f57a4fb90102ceaff27c5f62d458b72d20f89ea6
Rajendran Nadar <17236768+raajnadar@users.noreply.github.com>
2026-05-03 00:18:26 +0530

    initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,64 @@
+import js from '@eslint/js'
+import tsPlugin from '@typescript-eslint/eslint-plugin'
+import tsParser from '@typescript-eslint/parser'
+import reactPlugin from 'eslint-plugin-react'
+import reactHooksPlugin from 'eslint-plugin-react-hooks'
+import reactNativePlugin from 'eslint-plugin-react-native'
+import prettierConfig from 'eslint-config-prettier'
+
+export default [
+  {
+    ignores: [
+      '**/dist/**',
+      '**/.turbo/**',
+      '**/.expo/**',
+      '**/node_modules/**',
+      '**/build/**',
+      '**/coverage/**',
+    ],
+  },
+  js.configs.recommended,
+  {
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 2022,
+        sourceType: 'module',
+        ecmaFeatures: { jsx: true },
+      },
+      globals: {
+        __DEV__: 'readonly',
+        console: 'readonly',
+        global: 'readonly',
+        process: 'readonly',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+      react: reactPlugin,
+      'react-hooks': reactHooksPlugin,
+      'react-native': reactNativePlugin,
+    },
+    settings: {
+      react: { version: 'detect' },
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+      ...reactPlugin.configs.recommended.rules,
+      ...reactHooksPlugin.configs.recommended.rules,
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+      'react-native/no-inline-styles': 'error',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+      ],
+      '@typescript-eslint/consistent-type-imports': [
+        'error',
+        { prefer: 'type-imports', fixStyle: 'inline-type-imports' },
+      ],
+    },
+  },
+  prettierConfig,
+]
